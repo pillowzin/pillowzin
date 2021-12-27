00434676a545f1bf4996c2fa1114b4c2d6149746
pillowzin <96745954+pillowzin@users.noreply.github.com>
2021-12-27 16:14:10 -0300

    Create profile :).py

---
+++ profile :).py
@@ -0,0 +1,17 @@
+uh, hello
+
+I am jacob, aka "pillowzin"
+
+i don't know how to use this
+but
+i am learning python
+and i feel this is a good place to improve my
+programing abilities :)
+
+i started some months ago, but i really like
+to learn new tricks and codes
+
+<!---
+pillowzin/pillowzin is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
